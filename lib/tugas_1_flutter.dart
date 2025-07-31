87c5a062e57a527b8767bd079fc6cc3844e7d97e
Desty Alamanda <destyastyalamanda@gmail.com>
2025-07-31 11:46:08 +0700

    final task [Tugas 1 Flutter Widget]

--- lib/tugas_1_flutter.dart
+++ lib/tugas_1_flutter.dart
@@ -8,7 +8,10 @@ class TugasSatu extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Profil Saya", 
-        style: TextStyle(color: Colors.white, fontFamily : "LibertinusSerif" )
+        style: TextStyle(
+          color: Colors.white, 
+          fontFamily : "LibertinusSerif" 
+          )
         ),
         backgroundColor: Colors.deepPurple[200],
         centerTitle: true,
@@ -22,20 +25,30 @@ class TugasSatu extends StatelessWidget {
               fontSize: 25, 
               color: Colors.deepPurple[200],
               fontWeight: FontWeight.bold, 
-              fontFamily : "LibertinusSeriff")
+              fontFamily : "LibertinusSeriff"
+                )
               ),
               SizedBox(height: 20),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.location_on, color: Colors.deepPurple[200]), 
-              Text("Jakarta", style: TextStyle(color: Colors.deepPurple[200], fontWeight: FontWeight.bold)
+              Icon(Icons.location_on, 
+              color: Colors.deepPurple[200]
+              ),
+              Text("Jakarta", 
+              style: TextStyle(
+                color: Colors.deepPurple[200], 
+                fontWeight: FontWeight.bold
+                )
               )
             ],
           ),
           SizedBox(height: 20),
           Text("Seorang pelajar yang sedang belajar Flutter.",
-          style: TextStyle(color: Colors.deepPurple[200], fontWeight: FontWeight.bold)
+          style: TextStyle(
+            color: Colors.deepPurple[200], 
+            fontWeight: FontWeight.bold
+            )
           )
         ],
       ),
