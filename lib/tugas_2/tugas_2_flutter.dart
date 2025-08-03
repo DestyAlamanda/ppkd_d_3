7e4afa2a97b852a4b3b85e9e8bf6de7b7f121b41
DestyAlamanda <destyastyalamanda@gmail.com>
2025-08-03 20:49:08 +0700

    Final Task [Tugas 2 Flutter]

--- lib/tugas_2/tugas_2_flutter.dart
+++ lib/tugas_2/tugas_2_flutter.dart
@@ -8,22 +8,25 @@ class TugasDua extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Profil Lengkap", 
-        style: TextStyle(
-          color: Colors.white, 
-          )
-        ),
-        backgroundColor: Colors.grey,
+        style: TextStyle(color: Colors.grey[100])),
+        backgroundColor: Colors.grey[600],
         centerTitle: true,
       ),
       body: Column(
         children: [
           SizedBox(height: 30),
           CircleAvatar(
-            radius: 60,
-            backgroundImage: AssetImage("assets/photos/carmen.jpg",)
+            radius: 70,
+            backgroundImage: AssetImage("assets/photos/carmen2.jpg"),
           ),
-          SizedBox(height: 20),
-          Text("Desty Asty Alamanda", style: TextStyle(fontSize: 24, color: Color.fromARGB(255, 162, 162, 162))
+          SizedBox(height: 30),
+          Text(
+            "Desty Asty Alamanda",
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.grey[600],
+              fontWeight: FontWeight.bold
+            ),
           ),
           SizedBox(height: 20),
           Padding(
@@ -32,56 +35,69 @@ class TugasDua extends StatelessWidget {
               padding: EdgeInsets.all(12),
               decoration: BoxDecoration(
                 color: Colors.grey[300],
-                 border: Border.all(color: const Color.fromARGB(255, 162, 162, 162), width: 3),
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 2,
+                ),
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Row(
                 children: [
-                  SizedBox(width: 15),
-                  Icon(Icons.email, color: Colors.white),
-                  // SizedBox(width: 15),
+                  Icon(Icons.email, color: Colors.grey[700]),
                   Spacer(),
-                  Text("Destyastyalamanda@gmail.com", 
-                  style: TextStyle(fontSize: 20, 
-                  color: Colors.white,)
-                  )]
-              )  
-             )
-           ),
-           Padding(
-            padding: EdgeInsets.symmetric(vertical:20 ,horizontal: 20),
+                  Text(
+                    "Alamanda@gmail.com",
+                    style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 3, horizontal: 20),
             child: Container(
               padding: EdgeInsets.all(12),
               decoration: BoxDecoration(
                 color: Colors.grey[300],
-                 border: Border.all(color: const Color.fromARGB(255, 162, 162, 162), width: 3),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 162, 162, 162),
+                  width: 2,
+                ),
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Row(
                 children: [
-                  SizedBox(width: 15),
-                  Icon(Icons.phone,color: Colors.white),
-                  // SizedBox(width: 15),
+                  Icon(Icons.phone, color: Colors.grey[700]),
                   Spacer(),
-                  Text("0898-8518-083",
-                  style: TextStyle(fontSize: 20, 
-                  color: Colors.white,)
-                  )]
-              )  
-             )
-           ),
-            SizedBox(height: 50),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 100.0),
-              child: Row(
+                  Text(
+                    "0898-8518-083",
+                    style: TextStyle(fontSize: 20, color: Colors.grey[700])
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 25),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
               children: [
                 Expanded(
                   child: Container(
                     padding: EdgeInsets.all(12),
                     decoration: BoxDecoration(
                       color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(50)),
-                      child: Center(child: Text('Postingan', style: TextStyle(color: const Color.fromARGB(255, 94, 92, 92)))),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Postingan',
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
                   ),
                 ),
                 SizedBox(width: 10),
@@ -89,27 +105,45 @@ class TugasDua extends StatelessWidget {
                   child: Container(
                     padding: EdgeInsets.all(12),
                     decoration: BoxDecoration(
-                      color:const Color.fromARGB(255, 162, 162, 162),
-                      borderRadius: BorderRadius.circular(50)),
-                      child: Center(child: Text('Followers', style: TextStyle(color: Colors.grey[100]))),
+                      color: Colors.grey[500],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Followers',
+                        style: TextStyle(
+                          color: Colors.grey[100], 
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
             ),
           ),
-          SizedBox(height: 200),
-          Padding(padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Text("saya mahasiswa semester 7 jurusan teknik informatika yang sedang belajar flutter saya mahasiswa semester 7 jurusan teknik informatika yang sedang belajar fluttersaya mahasiswa semester 7 jurusan teknik informatika yang sedang belajar flutter "),
-          ), 
-           Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Container(
-                padding: EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                color: Colors.grey[300],
-                border: Border.all(color: const Color.fromARGB(255, 162, 162, 162), width: 3),
-                borderRadius: BorderRadius.circular(8),
-              ),
+          SizedBox(height: 50),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            child: Text(
+              "Saya mahasiswa semester tujuh jurusan teknik informatika yang sedang belajar mobile programming di PPKD Jakarta Pusat Batch 3 tahun 2025, Saya sangat senang dan bersemangat.  ",
+              textAlign: TextAlign.justify,
+              style: TextStyle(color: Colors.grey[800]),
+            ),
+          ),
+          Spacer(),
+          Container(
+            padding: EdgeInsets.only(top: 50),
+            decoration: BoxDecoration(
+              color: Colors.grey[600]),
+            child: Center(
+              // child: Text(
+              //   'PPKD Batch 3',
+              //   style: TextStyle(
+              //     color: const Color.fromARGB(255, 94, 92, 92),
+              //     fontSize: 16,
+              //   ),
+              // ),
             ),
           ),
         ],
