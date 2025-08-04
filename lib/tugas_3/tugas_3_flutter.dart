27785b51d447266732c53783493e98252d1016da
DestyAlamanda <destyastyalamanda@gmail.com>
2025-08-05 03:51:00 +0700

    fix error

--- lib/tugas_3/tugas_3_flutter.dart
+++ lib/tugas_3/tugas_3_flutter.dart
@@ -63,7 +63,7 @@ class TugasTiga extends StatelessWidget {
                     Text("Galeri",
                     style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.grey),
                     ),
-                    SizedBox(height: ),
+                    SizedBox(height: 5),
                     GridView.count(
                       shrinkWrap: true,
                       crossAxisCount: 2,
